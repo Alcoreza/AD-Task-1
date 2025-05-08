69f71a1c8ebbf0c5429d5279739a40b79e7c9442
Alcoreza <202310642@fit.edu.ph>
2025-05-08 18:07:15 +0800

    feat(categoriesPage) - added a css link for both global and local css

--- page/categoriesPage/index.php
+++ page/categoriesPage/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Food Categories</title>
+    <link rel="stylesheet" href="assets\css\global.css">
+    <link rel="stylesheet" href="page\categoriesPage\assets\css\categories.css">
 </head>
 <body>
     <div class="card">
