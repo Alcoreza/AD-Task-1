56945c3444a79cfb2870b0996aafc69d1fa8d3b2
Alcoreza <202310642@fit.edu.ph>
2025-05-08 17:11:45 +0800

    feat(factsPage) - added array for facts

--- page/factsPage/index.php
+++ page/factsPage/index.php
@@ -8,6 +8,18 @@
 <body>
     <div class="card">
         <h2>Food Facts</h2>
+
+        <?php
+            $facts = [
+                "Carrots were originally purple.",
+                "Apple float because they are 25% air.",
+                "Honey never spoils.",
+                "Bananas are berries, but strawberries aren't",
+                "The most stolen food in the world is cheese.",
+                "Cucumbers are 96% water."
+            ];
+        
+        ?>
     </div>
 </body>
 </html>
