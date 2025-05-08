2cb97399d964518cefa93a088339201425cf5d63
Alcoreza <202310642@fit.edu.ph>
2025-05-08 17:26:53 +0800

    feat(categoriesPage) - added the food array and the loops for the classification of healthy and junk foods

--- page/categoriesPage/index.php
+++ page/categoriesPage/index.php
@@ -6,6 +6,37 @@
     <title>Food Categories</title>
 </head>
 <body>
-    
+    <div class="card">
+        <h2>Food Categories</h2>
+
+        <?php
+            $foods = [
+                "Pizza"=> "Junk",
+                "Salad"=> "Healthy",
+                "Burger"=> "Junk",
+                "Sushi"=> "Healthy",
+                "Fried Chicken"=> "Junk",
+                "Fruit Bowl"=> "Healthy",
+                "Ramen"=> "Junk",
+                "Grilled Fish"=> "Healthy",
+            ];
+
+            echo "<h4>Healthy Foods</h4><ul>";
+            foreach ($foods as $name => $value) {
+                if($type == "Healthy"){
+                    echo "<li>$name</li>";
+                }
+            }
+            echo "</ul>";
+            echo "<h4>Junk Foods</h4><ul>";
+            foreach ($foods as $name => $type){
+                if ($type == "Junk"){
+                    echo "<li>$name</li>";
+                }
+            }
+            echo "</ul>";
+        
+        ?>
+    </div>
 </body>
 </html>
